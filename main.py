cbb0290e47affcde5391a573073f9c2d998c2e20
chaitanya6777 <168096245+chaitanya6777@users.noreply.github.com>
2024-04-26 22:48:21 +0530

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import home,search, contact,resources
+import home, search, contact, resources, quiz, account
 
 st.set_page_config(page_title="EduX", page_icon="🇮🇳", layout="wide")
 
@@ -17,14 +17,14 @@ def run():
     with st.sidebar:
         app = option_menu(
             menu_title='EduX',
-            options=['Home','Search','Contact Us','Resources'], 
-            icons=['house-fill','search', 'chat-fill','message','pushpin'],  
+            options=['Home', 'Account', 'Search', 'Contact Us', 'Resources', 'Quiz'], 
+            icons=['house-fill', 'person-fill', 'search', 'chat-fill', 'book-fill', 'pin-fill'],  
             menu_icon='chat-text-fill',
             default_index=0,
             styles={
                 "container": {"padding": "5!important", "background-colour": 'black'},  
                 "icon": {"colour": "white", "font-size": "23px"},  
-                "nav-link": {"colour": "white", "font-size": "20px", "text-align": "left", "margin": "0px" ,"--hover-color": "blue"},  
+                "nav-link": {"colour": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},  
                 "nav-link-selected": {"background-colour": "#02ab21"}, 
             }
         )
@@ -35,6 +35,11 @@ def run():
         search.app()
     elif app == 'Contact Us':
         contact.app()
-    elif app== "Resources":
+    elif app == "Resources":
         resources.app()
+    elif app == "Quiz":
+        quiz.app()
+    elif app == "Account":
+        account.app()
+
 run()
