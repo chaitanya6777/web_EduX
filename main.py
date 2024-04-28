c85650bc414da99fbe2bc804810f9a0c343edf03
chaitanya6777 <168096245+chaitanya6777@users.noreply.github.com>
2024-04-28 10:09:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import home, search, contact, resources, quiz, account
+import home, search, contact, resources, quiz, account,chatbot
 
 st.set_page_config(page_title="EduX", page_icon="🇮🇳", layout="wide")
 
@@ -17,7 +17,7 @@ def run():
     with st.sidebar:
         app = option_menu(
             menu_title='EduX',
-            options=['Home', 'Account', 'Search', 'Contact Us', 'Resources', 'Quiz'], 
+            options=['Home', 'Account', 'Search', 'Contact Us', 'Resources', 'Quiz','Ask Ai'], 
             icons=['house-fill', 'person-fill', 'search', 'chat-fill', 'book-fill', 'pin-fill'],  
             menu_icon='chat-text-fill',
             default_index=0,
@@ -41,5 +41,7 @@ def run():
         quiz.app()
     elif app == "Account":
         account.app()
-
+    elif app == "Ask Ai"
+        chatbot.app()
+    
 run()
