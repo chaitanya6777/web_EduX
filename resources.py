7fc1e16b498e2bf3b768e3a5abb220886149961e
chaitanya6777 <168096245+chaitanya6777@users.noreply.github.com>
2024-05-01 01:22:50 +0530

    Update resources.py

--- resources.py
+++ resources.py
@@ -1,21 +1,14 @@
-# resources.py
-
 import streamlit as st
 import os
 
-# Function to list all PDF files in the directory
 def list_pdf_files():
     pdf_files = [file for file in os.listdir("pdf_files") if file.endswith(".pdf")]
     return pdf_files
 
-# Define app for Resources section
 def app():
     st.title("Resources")
-
-    # List all PDF files in the directory
     pdf_files = list_pdf_files()
 
-    # Display PDF files in Resources section
     for pdf_file in pdf_files:
         st.markdown(f"### {pdf_file}")
         download_button = st.download_button(
